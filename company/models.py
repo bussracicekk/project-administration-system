b34a23185fbbb5ece7613225cb01d412e5a96ad4
Büşra <b21426807.cs.hacettepe.edu.tr>
2018-12-30 21:15:53 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Employee(models.Model):
     e_id = models.IntegerField(unique=True, primary_key=True, serialize=False, verbose_name='ID')
     e_name = models.CharField(max_length=30, verbose_name='Name')
@@ -19,11 +20,10 @@ class Head(models.Model):
 
 class Company(models.Model):
     c_id = models.IntegerField(unique=True, primary_key=True, verbose_name='ID')
-    c_name = models.CharField(max_length = 30, verbose_name='Company Name')
+    c_name = models.CharField(max_length=30, verbose_name='Company Name')
     c_email = models.CharField(max_length=30, verbose_name='Company E-mail')
     c_address = models.TextField(verbose_name='Company Address')
     c_password = models.CharField(max_length=6,  verbose_name='Company Password')
 
     def __str__(self):
         return self.c_name
-
