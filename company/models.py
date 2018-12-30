3a6c8c7fb3885dd376ddff2687ef7c766c1fb9e7
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-30 16:59:14 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    c_id = models.AutoField(unique=True, primary_key=True)
+    c_id = models.IntegerField(unique=True, primary_key=True)
     c_name =  models.CharField(max_length = 120, verbose_name='Company Name')
-    c_email = models.TextField(verbose_name='Company E-mail')
+    c_email = models.CharField(verbose_name='Company E-mail')
     c_address =  models.TextField(verbose_name='Company Address')
-    c_password =  models.TextField(max_length=10,  verbose_name='Company Password')
+    c_password =  models.CharField(max_length=10,  verbose_name='Company Password')
 
     def __str__(self):
         return self.c_name
