97e82c017cf536f1141987fe3a7784d56975c781
b21527381 <b21527381@cs.hacettepe.edu.tr>
2018-12-30 13:00:45 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Company(models.Model):
     c_id = models.AutoField(unique=True, primary_key=True)
-    c_name =  models.CharField(max_length = 120)
-    c_email = models.TextField()
-    c_address =  models.TextField()
-    c_password =  models.TextField(max_length=10)
+    c_name =  models.CharField(max_length = 120, verbose_name='Company Name')
+    c_email = models.TextField(verbose_name='Company E-mail')
+    c_address =  models.TextField(verbose_name='Company Address')
+    c_password =  models.TextField(max_length=10,  verbose_name='Company Password')
