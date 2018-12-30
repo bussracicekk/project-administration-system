f559489f3dce3ffa0f471fd73de561e71792ebf5
b21527381 <b21527381@cs.hacettepe.edu.tr>
2018-12-30 18:43:33 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -1,9 +1,26 @@
 from django.db import models
-
 # Create your models here.
+class Company(models.Model):
+    c_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Company ID')
+    c_name = models.CharField(max_length = 30, verbose_name='Company Name')
+    c_email = models.CharField(max_length=30, verbose_name='Company E-mail')
+    c_address = models.TextField(verbose_name='Company Address')
+    c_password = models.CharField(max_length=6,  verbose_name='Company Password')
+
+    def __str__(self):
+        return self.c_name , c_id
+
+class Department(models.Model):
+    d_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Department ID')
+    d_name = models.CharField(max_length=30, verbose_name='Department Name')
+    d_capacity = models.IntegerField(verbose_name='Department Capacity')
+    d_password = models.CharField(max_length=6, verbose_name='Department Password')
+
+    def __str__(self):
+        return self.d_name , d_id
 
 class Employee(models.Model):
-    e_id = models.IntegerField(unique=True, primary_key=True, serialize=False, verbose_name='ID')
+    e_id = models.IntegerField(unique=True, primary_key=True, serialize=False, verbose_name='Employee ID')
     e_name = models.CharField(max_length=30, verbose_name='Name')
     e_password = models.CharField(max_length=6, verbose_name='Password')
     e_email = models.CharField(max_length=30, verbose_name='Email')
@@ -11,19 +28,16 @@ class Employee(models.Model):
     e_degree = models.IntegerField(verbose_name='Degree')
     e_salary = models.IntegerField(verbose_name='Salary')
 
-
 class Head(models.Model):
     eHead = models.ForeignKey(Employee, unique=True, primary_key=True, serialize=False, verbose_name='Employee')
     h_degree = models.IntegerField(verbose_name='Head Degree')
 
 
-class Company(models.Model):
-    c_id = models.IntegerField(unique=True, primary_key=True, verbose_name='ID')
-    c_name = models.CharField(max_length = 30, verbose_name='Company Name')
-    c_email = models.CharField(max_length=30, verbose_name='Company E-mail')
-    c_address = models.TextField(verbose_name='Company Address')
-    c_password = models.CharField(max_length=6,  verbose_name='Company Password')
 
-    def __str__(self):
-        return self.c_name
+
+
+
+
+
+
 
