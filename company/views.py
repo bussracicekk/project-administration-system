7322a0c0f5332aebc04170e3f675eca1d3c24fe2
meltemtokgoz <meltemtgz@gmail.com>
2019-01-01 22:28:59 +0300

    Update views.py

--- company/views.py
+++ company/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
 from .models import Employee
 from .forms import EmployeeForm
+from .models import Department
+from .forms import DepartmentForm
 from django.contrib import messages
-
 
 def employee_index(request):
     employees = Employee.objects.all()
     return render(request, 'employee/index.html', {'employees': employees})
-
 
 def employee_detail(request, id):
     employees = get_object_or_404(Employee, e_id=id)
@@ -16,7 +16,6 @@ def employee_detail(request, id):
     }
 
     return render(request, 'employee/detail.html', context)
-
 
 def employee_create(request):
 
@@ -40,7 +39,6 @@ def employee_create(request):
     }
     return render(request, 'employee/form.html', context)
 
-
 def employee_update(request, id):
     employees = get_object_or_404(Employee, e_id=id)
     form = EmployeeForm(request.POST or None, instance=employees)
@@ -54,8 +52,51 @@ def employee_update(request, id):
     }
     return render(request, 'employee/update.html', context)
 
-
 def employee_delete(request, id):
     employees = get_object_or_404(Employee, e_id=id)
     employees.delete()
     return redirect('employee:index')
+
+##########################################################
+def department_index(request):
+    departments = Department.objects.all()
+    return render(request, 'department/index.html', {'departments': departments})
+
+def department_detail(request, id):
+    departments = get_object_or_404(Department, d_id=id)
+    context = {
+        'department': departments,
+    }
+
+    return render(request, 'department/detail.html', context)
+
+def department_create(request):
+    form = DepartmentForm(request.POST or None)
+    if form.is_valid():
+        departments = form.save()
+        messages.success(request, "Department is created, successfully!")
+        return HttpResponseRedirect(departments.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'department/form.html', context)
+
+
+def department_update(request, id):
+    departments = get_object_or_404(Department, d_id=id)
+    form = DepartmentForm(request.POST or None, instance=departments)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Department is updated, successfully!")
+        return HttpResponseRedirect(departments.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'department/update.html', context)
+
+def department_delete(request, id):
+    departments = get_object_or_404(Department, d_id=id)
+    departments.delete()
+    return redirect('department:index')
