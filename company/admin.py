9f37574868e5f5b08934ca7fccd636178631792c
b21527381 <b21527381@cs.hacettepe.edu.tr>
2019-01-01 12:36:13 +0300

    Update admin.py

--- company/admin.py
+++ company/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Employee
-from company.models import Employee
 from .models import Company
-
+from .models import Employee
+from .models import Head
+from .models import Department
+from .models import Project
+from .models import Issue
+from .models import Subtask
+#from company.models import Employee
 
 admin.site.register(Company)
 admin.site.register(Employee)
+admin.site.register(Head)
+admin.site.register(Department)
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Subtask)
+
 
 
