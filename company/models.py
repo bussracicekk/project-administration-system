50166408b657ad616eba93b8a2afdc96477ec044
meltemtokgoz <meltemtgz@gmail.com>
2019-01-01 13:09:57 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -16,13 +16,31 @@ class Department(models.Model):
     d_password = models.CharField(max_length=6, verbose_name='Department Password')
     dCompany = models.ForeignKey(Company,verbose_name='Company id' )
 
+class Employee(models.Model):
+    e_id = models.IntegerField(unique=True, primary_key=True, serialize=False, verbose_name='Employee ID')
+    e_name = models.CharField(max_length=30, verbose_name='Name')
+    e_password = models.CharField(max_length=6, verbose_name='Password')
+    e_email = models.CharField(max_length=30, verbose_name='Email')
+    e_phone = models.IntegerField(verbose_name='Phone')
+    e_degree = models.IntegerField(verbose_name='Degree')
+    e_salary = models.IntegerField(verbose_name='Salary')
+
 class Project(models.Model):
     p_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Project ID')
-    p_startdate = models.DateTimeField(verbose_name='Project Start Date Name')
-    p_enddate = models.DateTimeField(verbose_name='Project End Date Name')
+    p_startdate = models.DateTimeField(verbose_name='Project Start Date')
+    p_enddate = models.DateTimeField(verbose_name='Project End Date')
     p_title = models.CharField(max_length = 75, verbose_name = 'Project Title')
     p_situation = models.CharField(max_length = 20 , verbose_name= 'Project Situation')
     dProject = models.ForeignKey(Department, verbose_name = 'Deparment ID')
+
+class Other(models.Model):
+    o_studyproject = models.ForeignKey(Project,verbose_name = 'Works Project ID')
+    eOther = models.ForeignKey(Employee, unique=True, primary_key=True, serialize=False, verbose_name='Employee')
+
+class Head(models.Model):
+    eHead = models.ForeignKey(Employee, unique=True, primary_key=True, serialize=False, verbose_name='Employee')
+    h_degree = models.IntegerField(verbose_name='Head Degree')
+
 
 class Issue(models.Model):
     i_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Issue ID')
@@ -35,19 +53,21 @@ class Subtask(models.Model):
     sub_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Subtask ID')
     iIssue = models.ForeignKey(Issue, verbose_name = 'Issue ID')
 
-class Employee(models.Model):
-    e_id = models.IntegerField(unique=True, primary_key=True, serialize=False, verbose_name='Employee ID')
-    e_name = models.CharField(max_length=30, verbose_name='Name')
-    e_password = models.CharField(max_length=6, verbose_name='Password')
-    e_email = models.CharField(max_length=30, verbose_name='Email')
-    e_phone = models.IntegerField(verbose_name='Phone')
-    e_degree = models.IntegerField(verbose_name='Degree')
-    e_salary = models.IntegerField(verbose_name='Salary')
+class ProjectPlan(models.Model):
+    plan_id =  models.IntegerField(unique=True, primary_key=True, verbose_name='Plan ID')
+    plan_type = models.CharField(max_length=30, verbose_name='Plan Type')
+    plan_date = models.DateTimeField(verbose_name='Plan Date')
+    headMakes = models.ForeignKey(Head, verbose_name = 'Head ID')
+    pProjectPlan = models.ForeignKey(Project, verbose_name = 'Project ID')
 
-class Other(models.Model):
-    o_studyproject = models.ForeignKey(Project,verbose_name = 'Works Project ID')
-    eOther = models.ForeignKey(Employee, unique=True, primary_key=True, serialize=False, verbose_name='Employee')
+class WorkFlow():
+    w_type = models.CharField(max_length=30, verbose_name='Workflow Type')
+    w_date = models.DateTimeField(verbose_name='Workflow Date')
 
-class Head(models.Model):
-    eHead = models.ForeignKey(Employee, unique=True, primary_key=True, serialize=False, verbose_name='Employee')
-    h_degree = models.IntegerField(verbose_name='Head Degree')
+class Report():
+    r_type = models.CharField(max_length=30, verbose_name='Report Type')
+    r_version = models.CharField(max_length = 5 , verbose_name = "Report Version")
+    r_createdate = models.DateTimeField(verbose_name='Report Create Date')
+    pReport = models.ForeignKey(Project, verbose_name='Project ID')
+    oReport = models.ForeignKey(Other, verbose_name = 'Prepare Employee ID')
+
