d76420979a277f76dad3d53233f25f38dc2a2c11
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-01 19:02:35 +0300

    Update urls.py

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from .views import *
 
+
+app_name = 'employee'
+
+
 urlpatterns = [
 
-    url(r'^index/$', employee_index),
+    url(r'^employees/$', employee_index),
+    url(r'^(?P<id>\d+)/$', employee_detail, name="detail"),
+    url(r'^add/$', employee_create),
 
 ]
