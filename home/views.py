88f7a7ca6f40f4bc563df4cb3105a42f08e1a6cc
meltemtokgoz <meltemtgz@gmail.com>
2019-01-01 14:10:55 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-
-
 def home_view(request):
-    return HttpResponse('<b>Welcome</b>')
+    #return HttpResponse('<b>Welcome</b>')
+    return render(request, 'home/home.html', {})
