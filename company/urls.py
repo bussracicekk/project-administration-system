e5e08fb81574b6a83831a6b535df87e88cc8b78f
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-01 21:10:19 +0300

    Update urls.py

--- company/urls.py
+++ company/urls.py
@@ -7,9 +7,10 @@ app_name = 'employee'
 
 urlpatterns = [
 
-    url(r'^employees/$', employee_index),
+    url(r'^employees/$', employee_index, name='index'),
     url(r'^(?P<id>\d+)/detail/$', employee_detail, name="detail"),
-    url(r'^employee/add/$', employee_create),
-    url(r'^(?P<id>\d+)/update/$', employee_update),
+    url(r'^employee/add/$', employee_create, name='create'),
+    url(r'^(?P<id>\d+)/update/$', employee_update, name='update'),
+    url(r'^(?P<id>\d+)/delete/$', employee_delete, name='delete'),
 
 ]
