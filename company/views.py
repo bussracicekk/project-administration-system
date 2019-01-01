e9247192aa8b089169e7ddd29f9d3a0bb122bdd9
meltemtokgoz <meltemtgz@gmail.com>
2019-01-01 14:10:52 +0300

    Update views.py

--- company/views.py
+++ company/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, HttpResponse, get_list_or_404
 from .models import Employee
 
-# Create your views here.
-
 
 def employee_index(request):
     employees = Employee.objects.all()
