319b50e3a9e31ee8888ff46abd4d480bd8f19c67
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-01 19:02:07 +0300

    Update forms.py

--- company/forms.py
+++ company/forms.py
@@ -1,2 +1,18 @@
 from django import forms
 from .models import Employee
+
+
+class EmployeeForm(forms.ModelForm):
+
+    class Meta:
+        model = Employee
+        fields = [
+            'e_id',
+            'e_name',
+            'e_surname',
+            'e_password',
+            'e_email',
+            'e_phone',
+            'e_degree',
+            'e_salary',
+        ]
