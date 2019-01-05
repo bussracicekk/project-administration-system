7ccdbc4bce581943d6d63d29b9ee9d3d61de3485
meltemtokgoz <meltemtgz@gmail.com>
2019-01-05 17:27:07 +0300

    Update urls.py

--- company/urls.py
+++ company/urls.py
@@ -6,21 +6,21 @@ app_name = 'app'
 urlpatterns = [
 
     url(r'^employees/$', employee_index, name='index'),
-    url(r'^(?P<id>\d+)/detail/$', employee_detail, name="detail"),
     url(r'^employee/add/$', employee_create, name='create'),
-    url(r'^(?P<id>\d+)/update/$', employee_update, name='update'),
-    url(r'^(?P<id>\d+)/delete/$', employee_delete, name='delete'),
+    url(r'^(?P<e_slug>[\w-]+)/detail/$', employee_detail, name='detail'),
+    url(r'^(?P<e_slug>[\w-]+)/update/$', employee_update, name='update'),
+    url(r'^(?P<e_slug>[\w-]+)/delete/$', employee_delete, name='delete'),
 
     url(r'^departments/$', department_index, name='indexD'),
     url(r'^department/add/$', department_create, name='createD'),
-    url(r'^(?P<d_slug>[\w-]+)/detailD/$', department_detail, name="detailD"),
+    url(r'^(?P<d_slug>[\w-]+)/detailD/$', department_detail, name='detailD'),
     url(r'^(?P<d_slug>[\w-]+)/updateD/$', department_update, name='updateD'),
     url(r'^(?P<d_slug>[\w-]+)/deleteD/$', department_delete, name='deleteD'),
 
     url(r'^projects/$', project_index, name='indexP'),
     url(r'^project/add/$', project_create, name='createP'),
-    url(r'^(?P<id>\d+)/detailP/$', project_detail, name="detailP"),
-    url(r'^(?P<id>\d+)/updateP/$', project_update, name='updateP'),
-    url(r'^(?P<id>\d+)/deleteP/$', project_delete, name='deleteP'),
+    url(r'^(?P<p_slug>[\w-]+)/detailP/$', project_detail, name='detailP'),
+    url(r'^(?P<p_slug>[\w-]+)/updateP/$', project_update, name='updateP'),
+    url(r'^(?P<p_slug>[\w-]+)/deleteP/$', project_delete, name='deleteP'),
 
 ]
