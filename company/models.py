53ad7fc7be368feb023de6d8de13eb6d3729364e
meltemtokgoz <meltemtgz@gmail.com>
2019-01-05 17:27:04 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
@@ -7,7 +9,7 @@ class Company(models.Model):
     c_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Company ID')
     c_name = models.CharField(max_length=30, verbose_name='Company Name')
     c_email = models.CharField(max_length=30, verbose_name='Company E-mail')
-    c_address = models.TextField(verbose_name='Company Address')
+    c_address = RichTextField(verbose_name='Company Address')
     c_phone = models.CharField(max_length=20, verbose_name = 'Company Phone')
     c_password = models.CharField(max_length=6,  verbose_name='Company Password')
 
@@ -69,22 +71,35 @@ class Employee(models.Model):
     e_salary = models.IntegerField(verbose_name='Salary')
     eDepartment = models.ForeignKey(Department, verbose_name="Department Name")
     eCompany = models.ForeignKey(Company, verbose_name="Company Name")
-    e_slug = models.SlugField(unique=True)
+    e_slug = models.SlugField(unique=True,editable=False, max_length=130)
 
     def __str__(self):
         return self.e_name
 
     def get_absolute_url(self):
-        return reverse('app:detail', kwargs={'id': self.e_id})
+        return reverse('app:detail', kwargs={'e_slug': self.e_slug})
 
     def get_create_url(self):
         return reverse('app:create')
 
     def get_update_url(self):
-        return reverse('app:update', kwargs={'id': self.e_id})
+        return reverse('app:update', kwargs={'e_slug': self.e_slug})
 
     def get_delete_url(self):
-        return reverse('app:delete', kwargs={'id': self.e_id})
+        return reverse('app:delete', kwargs={'e_slug': self.e_slug})
+
+    def get_unique_E_slug(self):
+        e_slug = slugify(self.e_name.replace('ı', 'i'))
+        unique_slug = e_slug
+        counter = 1
+        while Employee.objects.filter(e_slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(e_slug, counter)
+            counter += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        self.e_slug = self.get_unique_E_slug()
+        return super(Employee, self).save(*args, **kwargs)
 
 
 class Project(models.Model):
@@ -95,22 +110,35 @@ class Project(models.Model):
     p_situation = models.CharField(max_length=20, verbose_name='Project Situation')
     dProject = models.ForeignKey(Department, verbose_name='Department Name')
     image = models.ImageField(null=True, blank=True)
-    p_slug = models.SlugField(unique=True)
+    p_slug = models.SlugField(unique=True,editable=False, max_length=130)
 
     def __str__(self):
         return self.p_title
 
     def get_project_url(self):
-        return reverse('app:detailP', kwargs={'id': self.p_id})
+        return reverse('app:detailP', kwargs={'p_slug': self.p_slug})
 
     def get_createP_url(self):
         return reverse('app:createP')
 
     def get_updateP_url(self):
-        return reverse('app:updateP', kwargs={'id': self.p_id})
+        return reverse('app:updateP', kwargs={'p_slug': self.p_slug})
 
     def get_deleteP_url(self):
-        return reverse('app:deleteP', kwargs={'id': self.p_id})
+        return reverse('app:deleteP', kwargs={'p_slug': self.p_slug})
+
+    def get_unique_P_slug(self):
+        p_slug = slugify(self.p_title.replace('ı', 'i'))
+        unique_slug = p_slug
+        counter = 1
+        while Project.objects.filter(p_slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(p_slug, counter)
+            counter += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        self.p_slug = self.get_unique_P_slug()
+        return super(Project, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['p_enddate']
@@ -137,8 +165,8 @@ class Head(models.Model):
 class Issue(models.Model):
     i_id = models.IntegerField(unique=True, primary_key=True, verbose_name='Issue ID')
     i_type = models.CharField(max_length=30, verbose_name='Issue Type')
-    i_extra = models.TextField(verbose_name='Issue Notes')
-    i_content = models.TextField(verbose_name='Issue Content')
+    i_extra = RichTextField(verbose_name='Issue Notes')
+    i_content = RichTextField(verbose_name='Issue Content')
     pIssue = models.ForeignKey(Project, verbose_name='Project ID')
 
 
