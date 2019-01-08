fd44e8d3e31e5bea6232cfdf5c4741536c3bb24e
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-08 19:25:19 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+
 def home_view(request):
     #return HttpResponse('<b>Welcome</b>')
     return render(request, 'home/home.html', {})
+
+
+def company_view(request):
+    return render(request, 'home/CompanyHome.html', {})
