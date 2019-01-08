4619afa3b69fa00fbb71533f2d50e873bd8fa3c8
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-08 19:19:44 +0300

    Add files via upload

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=15, label='User Name')
+    password = forms.CharField(max_length=10, label='Password', widget=forms.PasswordInput)
+    usertype = forms.CharField(max_length=10, label='User Type')
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        usertype = self.cleaned_data.get('usertype')
+        if username and password:
+            user = authenticate(username=username, password=password, useryype=usertype)
+            if not user:
+                raise forms.ValidationError('Username or password is not correct')
+            return  super(LoginForm, self).clean()
---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        usertype = form.cleaned_data.get('usertype')
+        user = authenticate(username=username, password=password, useryype=usertype)
+        login(request, user)
+        if usertype == "company":
+            return redirect('company')
+        if usertype == "customer":
+            return redirect('home')
+    return render(request, 'accounts/form.html', {'form': form})
