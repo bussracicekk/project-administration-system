e3cf4cc3a11dc3b896ebdd8dbcb02c2ad6de2a2d
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-09 04:30:27 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
@@ -12,10 +12,11 @@ def login_view(request):
         user = authenticate(username=username, password=password, useryype=usertype)
         login(request, user)
         if usertype == "company":
-            return redirect('home')
+            return redirect('app:homeC')
         if usertype == "customer":
             return redirect('home')
     return render(request, 'accounts/form.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
