7df91aed52365dced178ceb21c5e048acf5e9fbc
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-11 14:49:47 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 def login_view(request):
-    form = LoginForm(request.POST or None)
+    form = LoginForm(request.POST or None,  request.FILES or None)
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
@@ -21,3 +24,19 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('app:homeC')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/changepassword.html', {
+        'form': form
+    })
