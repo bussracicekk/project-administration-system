4a8c49b16747a5c74e17c6ff4408def6a1fa2c83
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2019-01-13 02:26:06 +0300

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import authenticate
 from django.db import transaction
+from django.contrib.auth.models import User
 ####################################################################
 
 ####################################################################
@@ -20,4 +21,25 @@ class LoginForm(forms.Form):
                 raise forms.ValidationError('Username or password is not correct')
             return super(LoginForm, self).clean()
 
+#####################################################################
+
+#####################################################################
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=30, label='Username')
+    password1 = forms.CharField(max_length=30, label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=30, label='Password Again', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password1',
+            'password2',
+        ]
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_date.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords are not the same")
+        return password2
 #####################################################################
